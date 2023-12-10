04abd81ffc6b5420aedb803ffec7c1f1f821c6a6
novikovanat <72397136+novikovanat@users.noreply.github.com>
2023-12-10 18:49:34 +0100

    html for first task added

--- js/task-1.js
+++ js/task-1.js
@@ -6,3 +6,8 @@ function makeTransaction(quantity, pricePerDroid) {
 console.log(makeTransaction(5, 3000)); // "You ordered 5 droids worth 15000 credits!"
 console.log(makeTransaction(3, 1000)); // "You ordered 3 droids worth 3000 credits!"
 console.log(makeTransaction(10, 500)); // "You ordered 10 droids worth 5000 credits!"
+
+document.getElementById("task-one-first").innerHTML = (makeTransaction(5, 3000));
+document.getElementById("task-one-second").innerHTML = (makeTransaction(3, 1000));
+document.getElementById("task-one-third").innerHTML = (makeTransaction(10, 500));
+
