aa3111ca28c0522b453210403a59bd20f9501ff0
novikovanat <72397136+novikovanat@users.noreply.github.com>
2023-12-10 19:51:32 +0100

    html for task 2,3 added

--- js/task-2.js
+++ js/task-2.js
@@ -5,3 +5,7 @@ function getShippingMessage(country, price, deliveryFee) {
 console.log(getShippingMessage('Australia', 120, 50)); // "Shipping to Australia will cost 170 credits"
 console.log(getShippingMessage('Germany', 80, 20)); // "Shipping to Germany will cost 100 credits"
 console.log(getShippingMessage('Sweden', 100, 20)); // "Shipping to Sweden will cost 120 credits"
+
+document.getElementById("task-two-first").innerHTML = (getShippingMessage('Australia', 120, 50));
+document.getElementById("task-two-second").innerHTML = (getShippingMessage('Germany', 80, 20));
+document.getElementById("task-two-third").innerHTML = (getShippingMessage('Sweden', 100, 20));
--- js/task-3.js
+++ js/task-3.js
@@ -10,3 +10,6 @@ console.log(getElementWidth('50px', '8px', '4px')); // 74
 console.log(getElementWidth('60px', '12px', '8.5px')); // 101
 console.log(getElementWidth('200px', '0px', '0px')); // 200
 
+document.getElementById("task-three-first").innerHTML = (getElementWidth('50px', '8px', '4px'));
+document.getElementById("task-three-second").innerHTML = (getElementWidth('60px', '12px', '8.5px'));
+document.getElementById("task-three-third").innerHTML = (getElementWidth('200px', '0px', '0px'));
